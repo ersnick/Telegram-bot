4b6314ca0644ffd3844c9b2ab376b0c702d2898a
Arseny <20mineslend14@gmail.com>
2023-06-28 23:57:41 +0500

    add logger config

--- user-service/main.py
+++ user-service/main.py
@@ -1,11 +1,27 @@
+import logging
+from logging import INFO
+
 from dotenv import load_dotenv
+
 from core.repositories.db.connections import get_db_connection
 
 
 def main():
-    get_db_connection()
+    logging.getLogger().info('test')
+    # get_db_connection()
+
+
+def __config_logger():
+    file_log = logging.FileHandler('user-service.log')
+    console_log = logging.StreamHandler()
+    FORMAT = '[%(levelname)s] %(asctime)s : %(message)s | %(filename)s'
+    logging.basicConfig(level=INFO,
+                        format=FORMAT,
+                        handlers=(file_log, console_log),
+                        datefmt='%d-%m-%y - %H:%M:%S')
 
 
 if __name__ == '__main__':
+    __config_logger()
     load_dotenv('../.env')
     main()
