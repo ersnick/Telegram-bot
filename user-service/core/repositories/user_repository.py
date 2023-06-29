81aa237ccaa84aacdd18344ad41ab2d3e9801449
Arseny <20mineslend14@gmail.com>
2023-06-29 18:21:50 +0500

    create user repository

--- user-service/core/repositories/user_repository.py
+++ user-service/core/repositories/user_repository.py
@@ -6,27 +6,27 @@ from sqlalchemy.orm import Session
 
 class UserRepository(ABC):
     @abstractmethod
-    def save_user(self, user: User):
+    def save_user(self, user: User) -> None:
         pass
 
     @abstractmethod
-    def get_user(self, user_id: int):
+    def get_user(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    def get_all_user(self):
+    def get_all_user(self) -> list[User]:
         pass
 
     @abstractmethod
-    def get_user_by_id(self, user_id: int):
+    def get_user_by_id(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    def get_user_by_username(self, username: str):
+    def get_user_by_username(self, username: str) -> User:
         pass
 
     @abstractmethod
-    def update_user_role(self, user_id: int, role_id):
+    def update_user_role(self, user_id: int, role_id) -> None:
         pass
 
 
@@ -34,22 +34,36 @@ class UserRepositoryImpl(UserRepository):
     def __init__(self, session: Session) -> None:
         self.__session = session
 
-    def get_user(self, user_id: int):
-        pass
+    def get_user(self, user_id: int) -> User:
+        with self.__session.begin():
+            user = self.__session.query(User).filter(User.id == user_id).one()
+            self.__session.commit()
+        return user
 
-    def get_all_user(self):
-        pass
+    def get_all_user(self) -> list[User]:
+        with self.__session.begin():
+            users = self.__session.query(User)
+            self.__session.commit()
+        return users
 
-    def get_user_by_id(self, user_id: int):
-        pass
+    def get_user_by_id(self, user_id: int) -> User:
+        with self.__session.begin():
+            user = self.__session.query(User).filter(User.id == user_id).one()
+            self.__session.commit()
+        return user
 
-    def get_user_by_username(self, username: str):
-        pass
+    def get_user_by_username(self, username: str) -> User:
+        with self.__session.begin():
+            user = self.__session.query(User).filter(User.username == username).one()
+            self.__session.commit()
+        return user
 
-    def save_user(self, user: User):
+    def save_user(self, user: User) -> None:
         with self.__session.begin():
             self.__session.add(user)
             self.__session.commit()
 
-    def update_user_role(self, user_id: int, role_id):
-        pass
+    def update_user_role(self, user_id: int, role_id) -> None:
+        with self.__session.begin():
+            self.__session.query(User).filter(User.id == user_id).update({'role_id': role_id})
+            self.__session.commit()
